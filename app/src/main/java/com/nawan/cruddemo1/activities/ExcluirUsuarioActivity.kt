e4b4ef3b4f9f87afb3ec5aa44c7758a8c8f6ffa8
Nawan743 <nawandx743@gmail.com>
2020-12-13 22:38:57 -0300

    Initial commit

---
+++ app/src/main/java/com/nawan/cruddemo1/activities/ExcluirUsuarioActivity.kt
@@ -0,0 +1,12 @@
+package com.nawan.cruddemo1.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nawan.cruddemo1.R
+
+class ExcluirUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_excluir_usuario)
+    }
+}
